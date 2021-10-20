54c22143ec48079c2815e108b6c545745c5e8f7f
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-10-20 10:58:32 +0530

    Update Hangman.js

--- src/Hangman.js
+++ src/Hangman.js
@@ -230,6 +230,6 @@ function imshow() {
 
 }
 
-window.onbeforeunload = function(){
-   window.alert("Exit?");
-}
+window.addEventListener('beforeunload', function(event) {
+  //do something here
+}, false);
