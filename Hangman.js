0f60d3dd6ae7101a16382f725bff69978f246da0
amitesh kumar <amiteshptn@gmail.com>
2022-10-20 19:55:14 +0530

    made site more responsive

--- Hangman.js
+++ Hangman.js
@@ -31,7 +31,14 @@ function play() {
   document.getElementById("logo").style.cssText = `Display:none`;
   document.querySelector("main").style = `
   Display:flex; 
-  flex-direction:column;`;
+  flex-direction:column;
+  `;
+  if(window.innerWidth < 1000){
+   document.querySelector('main').style.cssText =`
+    background : #7b7b7b url("https://raw.githubusercontent.com/Aatmaj-Zephyr/Hangman/main/WallpaperDog-743770.jpg");
+     background-blend-mode: multiply;
+   `
+  }
   document.getElementById("welcome_text").style.cssText = `Display:none`;
 
   start();
@@ -72,6 +79,16 @@ function start() {
      border-radius: 12px;
      transition-duration: 0.8s;
      font-size:70px;padding:10px;`;
+
+  if(window.innerWidth<1000){
+    document.getElementById("Word").style = `
+      cursor: url('./pokeball.png'),pointer;
+    color:blue;
+     background-color:Pink;
+     border-radius: 12px;
+     transition-duration: 0.8s;
+     font-size:2.7rem;`
+  }
 
   document.getElementById("Word").innerHTML = WordLetters.join(" ");
   //Display the word in the HTML file
@@ -216,7 +233,7 @@ function GameOver() {
   document.getElementById("Word").innerHTML = Word;
   // document.getElementById("Word").style = "text-shadow: 2px 2px black;color:Red;transition-duration: 1.0s;font-size:150px;";
   document.getElementById("Word").style =
-    "cursor: url('./pokeball.png'), pointer;text-shadow: 2px 2px black;color:Red;transition-duration: 1.0s;font-size:120px;";
+    "cursor: url('./pokeball.png'), pointer;text-shadow: 2px 2px black;color:Red;transition-duration: 1.0s;font-size:5rem;";
 
   // Pokemon name speech synthesis
   if ("speechSynthesis" in window) {
@@ -324,3 +341,24 @@ window.addEventListener(
   },
   false
 );
+
+const bg_pic = document.querySelector('.fg-pic');
+const front_text = document.querySelector('.fg-elements');
+const main = document.querySelector('main')
+if(window.innerWidth<1000){
+    bg_pic.remove();
+    front_text.innerHTML+= `
+    <div class="tooltip-div">
+      <button class="btn tooltip-btn" style="display:none" id="TurnsLeft"> </button>
+      <span class="tooltiptext">Turns left</span>
+   </div>
+   <a target="_blank" id="hyperlink">
+      <img id="image" style="transition-duration: 3.0s;display:none;width:5%">
+   </a>
+    <img id="pica-pic" src="./Assets/pikachu.png" alt="">
+    `
+  main.style = `
+  grid-template-columns:1fr;
+  `
+
+}
